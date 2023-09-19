0a4c54b9601d57b00fab2f6a7486ee06d2131b1a
Boris Vasilev <bvasilev@uwo.ca>
2023-09-18 22:32:22 -0400

    Added a terminal window

---
+++ terminalCmds.js
@@ -0,0 +1,78 @@
+var github = 'https://github.com/borisonekenobi';
+var linkedin = 'https://www.linkedin.com/in/boris-vasilev-0b1554203';
+var instagram = 'https://instagram.com/boris.kiril.vasilev';
+var email = 'mailto:boris.kiril.vasilev@gmail.com';
+
+whois = [
+	'<br>',
+	'Hello there!',
+	'I am Boris, I am currently studying to become a software engineer and working as one part time!',
+	'I am a huge nerd of anything Star Wars (and own several lightsabers) and I\'m a huge geek as well.',
+	'I\'ve been programming since I was very young and although starting off with JavaScript, I have learnt many programming',
+	'languages including Java, Python, C# and PHP. Recently, I\'ve been working on creating my own programming language too!',
+	'I hope you have a look around my portfolio as well as this terminal :)',
+	'<br>',
+	'<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+	'<br>'
+];
+
+social = [
+	'<br>',
+	'github         <a href="' + github + '" target="_blank">' + github + '</a>',
+	'linkedin       <a href="' + linkedin + '" target="_blank">' + linkedin + '</a>',
+	'instagram      <a href="' + instagram + '" target="_blank">' + instagram + '</a>',
+	'<br>'
+];
+
+easteregg = [
+	'<br>',
+	'You found another easteregg! 🥚',
+	'<br>'
+];
+
+eastereggs = [
+	'<br>',
+	'I hear you\'re looking for easter eggs...',
+	'I can\'t tell you where they are, but I know their codenames',
+	'These are the ones I know so far:',
+	'  - Tiny',
+	'  - Rainbow',
+	'  - Minikit',
+	'  - Hacker',
+	'Let me know where you find them... if you can',
+	'<br>'
+]
+
+projects = [
+	'<br>',
+	'Image Resizer  <a href="https://github.com/borisonekenobi/trafalgareng-image-resizer" target="_blank">Image resizer made in C# for Trafalgar Engineering Ltd.</a>',
+	'Platformer     A platformer game I made with a couple friends (Can\'t make repo public)',
+	'C Snap Snap    <a href="https://github.com/borisonekenobi/C-Snap-Snap" target="_blank">Visual IDE for C++ (similar to Scratch)</a>',
+	'EcoExplorer    <a href="https://github.com/borisonekenobi/TU20-2022" target="_blank">An Android app made to help track invasive species of plants and animals</a>',
+	'MConnect       <a href="https://github.com/borisonekenobi/HTN-2020-" target="_blank">A website to help connect people with mentors. Made during the 2020 Hack the North hackathon</a>',
+	'<br>'
+];
+
+help = [
+	'<br>',
+	'<span class="command">clear</span>          Clear terminal',
+	'<span class="command">eastereggs</span>     Maybe an easter egg?',
+	'<span class="command">help</span>           Displays all commands',
+	'<span class="command">history</span>        View command history',
+	'<span class="command">projects</span>       View coding projects',
+	'<span class="command">social</span>         Display social networks',
+	'<span class="command">whois</span>          Who I am',
+	'<br>',
+];
+
+banner = [
+	'██████╗  ██████╗ ██████╗ ██╗███████╗    ██╗   ██╗ █████╗ ███████╗██╗██╗     ███████╗██╗   ██╗',
+	'██╔══██╗██╔═══██╗██╔══██╗██║██╔════╝    ██║   ██║██╔══██╗██╔════╝██║██║     ██╔════╝██║   ██║',
+	'██████╔╝██║   ██║██████╔╝██║███████╗    ██║   ██║███████║███████╗██║██║     █████╗  ██║   ██║',
+	'██╔══██╗██║   ██║██╔══██╗██║╚════██║    ╚██╗ ██╔╝██╔══██║╚════██║██║██║     ██╔══╝  ╚██╗ ██╔╝',
+	'██████╔╝╚██████╔╝██║  ██║██║███████║     ╚████╔╝ ██║  ██║███████║██║███████╗███████╗ ╚████╔╝ ',
+	'╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝╚══════╝      ╚═══╝  ╚═╝  ╚═╝╚══════╝╚═╝╚══════╝╚══════╝  ╚═══╝  ',
+	'                                                                                             ',
+	'<span class="color2">Welcome to the terminal.</span>',
+	'<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>'
+];
